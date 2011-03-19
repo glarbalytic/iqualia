c4ca79fc3562522f9c9d307dee597a6a11395be5
glarbalytic <jschmitz65@gmail.com>
2011-03-19 00:53:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iqualia::Application.config.secret_token = 'bb180172114778054b4823bd29e53fff4054fd472d08d85107fe93eca98ba9eba3cbc43830ad0c32ffbf7ac3fe09ef16e2f1f290b13ef6edb0c771224ae3e610'
