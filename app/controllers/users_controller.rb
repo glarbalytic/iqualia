109b015168a1402891a07349026059cd2e562356
glarbalytic <jschmitz65@gmail.com>
2011-03-19 23:54:18 -0700

    Done with sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to IQualia!"
       redirect_to @user
     else
